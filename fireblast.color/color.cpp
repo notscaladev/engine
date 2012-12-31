d4c6dd8ae5da394b5116e6778d505d683a6b6ca6
Viacheslav Okolitiy <justforgit@gmail.com>
2012-12-31 02:00:58 +0200

    partial implementation. Still in progress

--- fireblast.color/color.cpp
+++ fireblast.color/color.cpp
@@ -3,4 +3,67 @@
 
 class CColor : public Color
 {
+public:
+	Color getWhite()
+	{
+		Color* whiteColor = new Color(1, 1, 1, 1);
+		return whiteColor;
+		delete whiteColor;
+	};
+
+	Color getBlack()
+	{
+		Color* blackColor = new Color(0, 0, 0, 1);
+		return blackColor;
+		delete blackColor;
+	};
+
+	Color getRed()
+	{
+		Color* redColor = new Color(1, 0, 0, 1);
+		return redColor;
+		delete redColor;
+	}
+
+	Color getYellow()
+	{
+		Color* yellowColor = new Color(1, 1, 0, 1);
+		return yellowColor;
+		delete yellowColor;
+	}
+
+	Color getGreen()
+	{
+		Color* greenColor = new Color(0, 1, 0, 1);
+		return greenColor;
+		delete greenColor;
+	}
+
+	Color getCyan()
+	{
+		Color* cyanColor = new Color(0, 1, 1, 1);
+		return cyanColor;
+		delete cyanColor;
+	}
+
+	Color getBlue()
+	{
+		Color* blueColor = new Color(0, 0, 1, 1);
+		return blueColor;
+		delete blueColor;
+	}
+
+	Color getPink()
+	{
+		Color* pinkColor = new Color(1, 0, 1, 1);
+		return pinkColor;
+		delete pinkColor;
+	}
+
+	Color getTransparent()
+	{
+		Color* transparent = new Color(0, 0, 0, 0);
+		return transparent;
+		delete transparent;
+	}
 };
